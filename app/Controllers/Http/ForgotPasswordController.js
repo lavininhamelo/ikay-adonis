9dbdea7a1032be838e1eed41ec3c28612c951c91
Lavínia <lavininhamelo@hotmail.com>
2019-10-08 17:57:37 -0400

    Mail Config

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,7 +2,7 @@
 
 const crypto = require('crypto');
 const User = use('App/Models/User');
-
+const Mail = use('Mail');
 class ForgotPasswordController {
   async store({request, response}){
     try{
@@ -10,13 +10,31 @@ class ForgotPasswordController {
       const user = await User.findByOrFail('email', email);
       user.token = crypto.randomBytes(10).toString('hex');
       user.token_created_at = new Date();
+      console.log('Salvar');
       await user.save();
-    }catch(err){
-      return response
-      .status(err.status)
-      .send({error: {message: 'Algo não deu certo, esse e-mail existe?'}})
+      console.log('Enviar');
+   // Envia e-mail para reset de senha
+   await Mail.send(
+    ['emails.recover'],
+    {
+      email,
+      token: user.token,
+      link: `${request.input('request_url')}?token${user.token}`
+    },
+    message => {
+      message
+        .to(user.email)
+        .from('laviniascmelo@gmail.com', 'Lavínia Melo')
+        .subject('Recuperação de senha')
     }
-  };
+  )
+} catch (err) {
+  return response.status(err.status).send({
+    error: {
+      message: 'Algo deu errado. Verifique o e-mail e tente novamente'
+    }
+  })
+}}
 }
 
 module.exports = ForgotPasswordController
