24809f3d4678fcbee7224da769a93d6d261c2604
ianito <iaan.developer@gmail.com>
2019-12-11 15:48:30 -0300

    Co-authored-by: lavininhamelo <lavininhamelo@gmail.com>

--- app/Models/Hooks/OrderHook.js
+++ app/Models/Hooks/OrderHook.js
@@ -1,13 +1,23 @@
 "use strict";
 const Purchase = use("App/Models/Purchase");
-const OrderHook = (exports = module.exports = {});
+const Database = use("Database");
 const OrderProduct = use("App/Models/OrderProduct");
 const STATUS = { PENDING: 1, WAITING_PAYMENT: 2, OK: 3 };
+const OrderHook = (exports = module.exports = {});
+
+async function checkIfProductExists(user_id, product_id) {
+  const purchase = await Database.table("purchases")
+    .where({
+      product_id: product_id,
+      user_id: user_id
+    })
+    .first();
+  return purchase;
+}
 
 OrderHook.statusChanged = async order => {
   if (order.status_id === STATUS.OK) {
     //Se o status === PAGO, então...
-
     for (const data of await order.toJSON().products) {
       const { id } = await order.toJSON().users;
       const order_product = await OrderProduct.query()
@@ -16,13 +26,25 @@ OrderHook.statusChanged = async order => {
           order_id: data.pivot.order_id
         })
         .fetch();
+
       const { qntd } = await order_product.toJSON()[0];
 
-      const purchase = await Purchase.create({
-        user_id: id,
-        product_id: data.id,
-        remaining_uses: qntd
-      });
+      const isExists = await checkIfProductExists(id, data.pivot.product_id);
+      if (isExists) {
+        const total = qntd + isExists.remaining_uses;
+        const purchase = await Database.table("purchases")
+          .where({
+            product_id: data.pivot.product_id,
+            user_id: id
+          })
+          .update({ remaining_uses: total });
+      } else {
+        const purchase = await Purchase.create({
+          user_id: id,
+          product_id: data.id,
+          remaining_uses: qntd
+        });
+      }
     }
   }
 };
